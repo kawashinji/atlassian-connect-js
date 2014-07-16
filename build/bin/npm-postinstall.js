18199830a0cda7f2c228f805888c8a0ccd9f80ee
Chris Whittington <cwhittington@atlassian.com>
2014-07-16 16:39:45 +1000

    build automatically when installed

--- build/bin/npm-postinstall.js
+++ build/bin/npm-postinstall.js
@@ -1,5 +1,9 @@
 var helper = require('./bin-helper');
 
 helper.chain([
-    [helper.npmNormalize('./node_modules/.bin/bower'), ['install']]
+    [helper.npmNormalize('./node_modules/.bin/bower'), ['install']],
+    [helper.npmNormalize('./node_modules/grunt-cli/bin/grunt'),
+        ['--gruntfile', helper.npmNormalize('./Gruntfile.js'), 'build']
+    ]
+
 ]);
