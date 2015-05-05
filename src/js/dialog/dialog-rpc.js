b7e52aa04d65e7cbf9c229f6d1c0866f375170b4
James Hazelwood <jhazelwood@atlassian.com>
2015-05-05 12:44:32 +1000

    ACJS-22 Naieve focusing of iframe on dialog load

--- src/js/dialog/dialog-rpc.js
+++ src/js/dialog/dialog-rpc.js
@@ -24,6 +24,7 @@
                                 });
                             });
                         }
+                        xdm.iframe.focus();
                     }
                 },
                 internals: {
