049c49e2d447baef34b3cfe4af96304c5cf4fc4b
Filip Rogaczewski <frogaczewski@atlassian.com>
2015-09-16 14:16:39 +0200

    ACJIRA-220: PR comments

--- src/js/iframe/host/resize.js
+++ src/js/iframe/host/resize.js
@@ -21,12 +21,10 @@ define("resize", ["_dollar", "_rpc"], function ($, rpc) {
                     var resizeHandler = function (iframe) {
                         var height;
                         if (hideFooter) {
-                            //$('.ac-content-page #footer').append('<style>.class {display: none;}</style>');
                             $('.ac-content-page #footer').css({display: "none"});
                             $('.ac-content-page').css({overflow: "hidden !important"});
                             height = $(document).height() - $("#header > nav").outerHeight();
-                        }
-                        else {
+                        } else {
                             height = $(document).height() - $("#header > nav").outerHeight() - $("#footer").outerHeight() - 20;
                         }
 
