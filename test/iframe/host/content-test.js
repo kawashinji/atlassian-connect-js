bc53d66b77037a155dec151f1e2909a5a8ae7a66
Michael Staas <mstaas@atlassian.com>
2015-09-17 14:55:49 +1000

    fix test

--- test/iframe/host/content-test.js
+++ test/iframe/host/content-test.js
@@ -15,21 +15,21 @@ QUnit.module("Content Utilities", {
         this.server.respondWith("GET", new RegExp(".*" + contentPath + ".*"),
             [200, { "Content-Type": "text/html" }, 'This is the <span id="my-span">content</span>']);
 
-        $('<a id="qunit-fixture" href="http://foo.com/?width=101&height=200&cp=%2Fconfluence" class="ap-plugin-key-my-plugin ap-module-key-my-module" />').appendTo('body');
+        $('<a id="qunit-fixture-link" href="http://foo.com/?width=101&height=200&cp=%2Fconfluence" class="ap-plugin-key-my-plugin ap-module-key-my-module" />').appendTo('body');
     },
     teardown: function() {
         this.container.remove();
         delete AJS.contextPath;
         this.server.restore();
-        $('#qunit-fixture').remove();
+        $('#qunit-fixture-link').remove();
     },
 });
 
 QUnit.test("eventHandler assigns an event handler to the dom node", function(assert){
     var spy = sinon.spy();
 
-    contentUtilities.eventHandler("click", '#qunit-fixture', spy);
-    $('#qunit-fixture').trigger('click');
+    contentUtilities.eventHandler("click", '#qunit-fixture-link', spy);
+    $('#qunit-fixture-link').trigger('click');
 
     assert.ok(spy.calledOnce);
 });
@@ -37,8 +37,8 @@ QUnit.test("eventHandler assigns an event handler to the dom node", function(ass
 QUnit.test("eventHandler callback includes the width", function(assert){
     var spy = sinon.spy();
 
-    contentUtilities.eventHandler("click", '#qunit-fixture', spy);
-    $('#qunit-fixture').trigger('click');
+    contentUtilities.eventHandler("click", '#qunit-fixture-link', spy);
+    $('#qunit-fixture-link').trigger('click');
 
     assert.equal(spy.firstCall.args[1]['width'], '101');
 });
@@ -46,8 +46,8 @@ QUnit.test("eventHandler callback includes the width", function(assert){
 QUnit.test("eventHandler callback includes the height", function(assert){
     var spy = sinon.spy();
 
-    contentUtilities.eventHandler("click", '#qunit-fixture', spy);
-    $('#qunit-fixture').trigger('click');
+    contentUtilities.eventHandler("click", '#qunit-fixture-link', spy);
+    $('#qunit-fixture-link').trigger('click');
 
     assert.equal(spy.firstCall.args[1]['height'], '200');
 });
@@ -55,8 +55,8 @@ QUnit.test("eventHandler callback includes the height", function(assert){
 QUnit.test("eventHandler callback includes the context path", function(assert){
     var spy = sinon.spy();
 
-    contentUtilities.eventHandler("click", '#qunit-fixture', spy);
-    $('#qunit-fixture').trigger('click');
+    contentUtilities.eventHandler("click", '#qunit-fixture-link', spy);
+    $('#qunit-fixture-link').trigger('click');
 
     assert.equal(spy.firstCall.args[1]['cp'], '/confluence');
 
