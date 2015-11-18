e001dc04f50d1a730f78bfc5e8d63426ec087139
Kate West-Walker <kwestwalker@atlassian.com>
2015-11-18 18:30:45 +1100

    CE-249 Uri templating now psuedo works with the inbuilt uri library

--- src/js/navigation/main.js
+++ src/js/navigation/main.js
@@ -1,53 +1,58 @@
-(function(define, require, AJS, $){
+(function(define, AJS){
     "use strict";
-    define("ac/navigation", function() {
-        console.log("navigation main is loaded");
+    define("ac/navigation", ["connect-host"], function(connect) {
 
         if(!AJS.Confluence) {
-            console.warn('The navigation API is currently only implemented in Confluence.');
+            console.error('The navigation API is currently only implemented in Confluence.');
             return;
         }
 
-        require('urijs', function(urijs) {
-            /**
-             * limitations of the spike:
-             * 1. No final decision on the external representation of the API yet
-             * 2. Only handles a single wildcard in the url (so no create page yet) // solved with npm library
-             * 3. Needs the routes list pushed out to an external file
-             * 4. Doesn't handle a single parameter eg navigateTo("dashboard")
-             **/
-            var baseUrl = AJS.General.getBaseUrl();
+        var Uri = connect._uriHelper;
 
-            var routes = {
-                "contentview": "/pages/viewpage.action?pageId={id}",
-                "contentedit": "/pages/resumedraft.action?draftId={id}&draftShareId={shareToken}",
-                "dashboard"  : ""
-            };
+        /**
+         * limitations of the spike:
+         * 1. No final decision on the external representation of the API yet
+         * 2. Only handles a single wildcard in the url (so no create page yet) // solved with npm library
+         * 3. Needs the routes list pushed out to an external file
+         * 4. Doesn't handle a single parameter eg navigateTo("dashboard")
+         **/
+        var baseUrl = AJS.General.getBaseUrl();
 
-            var to = function (target, context) {
-                if (target in routes) {
-                    context = (typeof context === 'undefined') ? {} : context;
-                    document.location = buildUrl(routes[target], context);
-                } else {
-                    // error thing goes here
-                }
-            };
+        var routes = {
+            "contentview": "/pages/viewpage.action?pageId={id}",
+            "contentedit": "/pages/resumedraft.action?draftId={id}&draftShareId={shareToken}",
+            "dashboard"  : ""
+        };
 
-            var buildUrl = function (urlTemplate, context) {
-                //console.log("context = "+ context);
-                //console.log("urlTemplate = "+ urlTemplate);
-                var concreteUrl = urijs.URI.expand(urlTemplate, context);
-                console.log("concreteUrl = " + concreteUrl);
+        var to = function (target, context) {
+            if (target in routes) {
+                context = (typeof context === 'undefined') ? {} : context;
+                document.location = buildUrl(routes[target], context);
+            } else {
+                // error thing goes here
+            }
+        };
 
-                if (concreteUrl.contains('{') || concreteUrl.contains('}')) {
-                    console.log("error expanding url");
-                    // error condition because incorrect parameters, return expected
-                }
-            };
+        var buildUrl = function (urlTemplate, context) {
+            var concreteUrl = new Uri.init(urlTemplate);
+            var key;
 
-            return {
-                to: to
-            };
-        });
+            for (key in context.keys) {
+                concreteUrl = concreteUrl.replaceQueryParam(key, context[key]);
+            }
+            var stringUrl = concreteUrl.toString();
+
+            if (stringUrl.contains('{') || stringUrl.contains('}')) {
+                console.log("error expanding url");
+                // error condition because incorrect parameters, return expected
+            }
+
+            return concreteUrl.toString();
+        };
+
+        return {
+            to: to
+        };
     });
-})(define, require, AJS, AJS.$);
+})(define, AJS);
+
