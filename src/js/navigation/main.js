eac51fd5d93cc1c4f50bd9d5e2e17b47b3cae238
Kate West-Walker <kwestwalker@atlassian.com>
2015-11-19 15:21:58 +1100

    CE-249 Updating the implementation of the navigate method to use the urlTemplate library, and cleaning it up

--- src/js/navigation/main.js
+++ src/js/navigation/main.js
@@ -7,47 +7,45 @@
             return;
         }
 
-        var Uri = connect._uriHelper;
+        // ACJS-77: Migrate _uriTemplateHelper to use urijs.
+        var urijs   = connect._uriTemplateHelper,
+            baseUrl = AJS.General.getBaseUrl(),
 
-        /**
-         * limitations of the spike:
-         * 1. No final decision on the external representation of the API yet
-         * 2. Only handles a single wildcard in the url (so no create page yet) // solved with npm library
-         * 3. Needs the routes list pushed out to an external file
-         * 4. Doesn't handle a single parameter eg navigateTo("dashboard")
-         **/
-        var baseUrl = AJS.General.getBaseUrl();
-
-        var routes = {
-            "contentview": "/pages/viewpage.action?pageId={id}",
-            "contentedit": "/pages/resumedraft.action?draftId={id}&draftShareId={shareToken}",
-            "dashboard"  : ""
-        };
+            // TODO move these out to a json file
+            routes  = {
+                "dashboard"    : "",
+                "contentview"  : "/pages/viewpage.action?pageId={id}",
+                "contentedit"  : "/pages/resumedraft.action?draftId={id}&draftShareId={shareToken}",
+                "spaceview"    : "/display/CP/{id}",
+                "spaceadmin"   : "/spaces/viewspacesummary.action?key={id}",
+                "userprofile"  : "wiki/display/~{id}"
+                // TODO currentpage
+            };
 
         var to = function (target, context) {
             if (target in routes) {
                 context = (typeof context === 'undefined') ? {} : context;
                 document.location = buildUrl(routes[target], context);
             } else {
-                // error thing goes here
+                console.error("Unrecognised url target");
             }
         };
 
         var buildUrl = function (urlTemplate, context) {
-            var concreteUrl = new Uri.init(urlTemplate);
-            var key;
+            var concreteUrl;
 
-            for (key in context.keys) {
-                concreteUrl = concreteUrl.replaceQueryParam(key, context[key]);
-            }
-            var stringUrl = concreteUrl.toString();
-
-            if (stringUrl.contains('{') || stringUrl.contains('}')) {
-                console.log("error expanding url");
-                // error condition because incorrect parameters, return expected
+            if (context) {
+                concreteUrl = baseUrl + urijs.parse(urlTemplate).expand(context);
+            } else {
+                concreteUrl = baseUrl + urlTemplate;
             }
 
-            return concreteUrl.toString();
+            if (concreteUrl.indexOf('{') < -1 || concreteUrl.indexOf('}') < -1) {
+                console.error("Incorrect parameters to url " + urlTemplate);
+                return;
+            }
+
+            return concreteUrl;
         };
 
         return {
