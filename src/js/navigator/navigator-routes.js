4f0db2e1888b986e0410468b71237c51219cd6e4
Kate West-Walker <kwestwalker@atlassian.com>
2015-12-04 16:07:52 +1100

    CE-249 adding comment

--- src/js/navigator/navigator-routes.js
+++ src/js/navigator/navigator-routes.js
@@ -2,7 +2,11 @@
     "use strict";
     define("ac/navigator/navigator-routes", function() {
 
-        // Confluence only at the moment because JIRA navigation is not implemented yet.
+        /*
+        Confluence only at the moment because JIRA navigation is not implemented yet.
+        In future we also will pull this Confluence specific routing information out of Connect, and instead have the
+        method call delegate directly to the product. Tracked in CONFDEV-38620.
+         */
         var confluence_routes = {
             "dashboard"    : "",
             "contentview"  : "/pages/viewpage.action?pageId={contentId}",
