ad6f89b3936df7ff27d45af4e2a83392b10e4bd1
Kate West-Walker <kwestwalker@atlassian.com>
2015-12-09 11:46:32 +1100

    CE-249 Addressing PR comments on docs

--- src/js/iframe/plugin/navigator.js
+++ src/js/iframe/plugin/navigator.js
@@ -3,13 +3,6 @@ AP.define("navigator", ["_dollar", "_rpc"],
     /**
      * The Navigator API allows your add-on to change the current page using Javascript.
      *
-     * ### Example ###
-     * ```
-     * AP.require('navigator', function(navigator){
-     *   navigator.go('dashboard');
-     * });
-     * ```
-     *
      * Note: This API is currently only implemented for Confluence.
      *
      * @exports Navigator
@@ -25,18 +18,29 @@ AP.define("navigator", ["_dollar", "_rpc"],
              * @property {String}        contentview        The view page for pages and blogs. Takes a `contentId` to identify the content.
              * @property {String}        contentedit        The edit page for pages and blogs. Takes a `draftId` to identify the content and a `shareToken` to allow editing.
              * @property {String}        spaceview          The space view page. Takes a `spaceKey` to identify the space.
-             * @property {String}        spaceadmin         The space admin page. Takes a `spaceKey` to identify the space.
+             * @property {String}        spacetools         The space tools page. Takes a `spaceKey` to identify the space.
              * @property {String}        dashboard          The dashboard of Confluence.
              * @property {String}        userprofile        The profile page for a specific user. Takes a `username` to identify the user.
              */
 
             /**
-             * Navigates the user from the current page to the specified page. This call is made from within your iframe, out to the parent page.
+             * @class Navigator~context
+             * @property {String}        contentId          Identifies a piece of content. Required for the contentview target.
+             * @property {String}        shareToken         A token required to access a draft piece of content. Required for the contentedit target.
+             * @property {String}        draftId            Identifies a draft piece of content. Required for the contentedit target.
+             * @property {String}        spaceKey           Identifies a space. Required for the spaceview and spacetools targets.
+             * @property {String}        username           Identifies a user. Required for the userprofile target.
+             */
+
+            /**
+             * Navigates the user from the current page to the specified page. This call navigates the host product, not the iframe content.
+             *
+             * Requires a target location and the corresponding context. Navigating by passing a concrete url is currently unsupported.
              * @name go
              * @method
              * @memberof module:Navigator#
-             * @param {String} target The type of page to navigate to.
-             * @param {Object} context Specific information that identifies the page to navigate to.
+             * @param {Navigator~target} target The type of page to navigate to.
+             * @param {Navigator~context} context Specific information that identifies the page to navigate to.
              * @noDemo
              * @example
              * AP.require('navigator', function(navigator){
