41b56be43238d0e96b83be7823b63f505eb469e6
Jake Furler <jfurler@atlassian.com>
2015-12-09 14:51:20 +1100

    CE-249 moved routes to connect plugin js

--- src/js/navigator/main.js
+++ src/js/navigator/main.js
@@ -1,17 +1,21 @@
 (function (define, AJS) {
     "use strict";
-    define("ac/navigator", ["connect-host", "ac/navigator/navigator-routes", "ac/navigator-browser", "_uritemplate"], function (connect, navigatorRoutes, browser, uri) {
+    define("ac/navigator", ["connect-host", "ac/navigator-browser", "_uritemplate"], function (connect, browser, uri) {
 
         if (!AJS.Confluence) {
-            console.error('The navigator API is currently only implemented in Confluence.');
+            AJS.error('The navigator API is currently only implemented in Confluence.');
             return;
         }
 
         // ACJS-77: Migrate _uriTemplateHelper to use urijs.
         var baseUrl = AJS.General.getBaseUrl(),
-            routes = navigatorRoutes.routes;
+            routes = {};
 
         var go = function (target, context) {
+            if(Object.getOwnPropertyNames(routes).length === 0) {
+                AJS.error("No routes defined");
+                return;
+            }
             if (target in routes) {
                 context = context || {};
                 browser.goToUrl(buildUrl(routes[target], context));
@@ -31,10 +35,15 @@
 
             return baseUrl + uri.parse(urlTemplate).expand(context);
         };
+        
+        var setRoutes = function(r) {
+            routes = r;
+        };
 
         return {
             go: go,
-            reload: reload
+            reload: reload,
+            setRoutes: setRoutes
         };
     });
 })(define, AJS);
