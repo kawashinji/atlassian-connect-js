e444451af423aab232ef4cdb5fb19c81d2082ea7
Michael Staas <mstaas@atlassian.com>
2016-01-11 19:28:38 +1100

    Revert "Trying to get master FECQ build working"
    This reverts commit 307b0e0c002561b897ca33b3cce8c5cb9a62e774.

--- fecq.groovy
+++ fecq.groovy
@@ -1,6 +1,6 @@
 plan(key:'FECQ',name:'Front-End Code Quality Build',description:'Runs some ESLint rules on the repo, and pushes the results through git-ratchet.') {
 
-  repoName = 'Atlassian Connect JS'
+  repoName = 'Atlassian Connect JS (master)'
   repoUrl = 'ssh://git@stash.atlassian.com:7997/ac/atlassian-connect-js.git'
 
   project(key:'CONNECT',name:'Connect')
