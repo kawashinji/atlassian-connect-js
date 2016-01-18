3e46c674c469d3d2c38789541b5bb84236164187
Chris Whittington <cwhittington@atlassian.com>
2016-01-18 22:45:17 +0000

    update dist

--- src/host/index.js
+++ src/host/index.js
@@ -1,22 +1,13 @@
 import EventDispatcher from 'dispatchers/event_dispatcher';
 import simpleXDM from 'simple-xdm/dist/host';
 import jwtActions from 'actions/jwt_actions';
-
 import events from './extensions/events';
-// import content from './content';
 import create from './create';
 import dialog from './extensions/dialog';
 import env from './extensions/env';
-// import dialogBinder from './dialog/binder';
-// import dialogRpc from './dialog/rpc';
-// import inlineDialog from './inline-dialog/rpc';
-// import inlineDialogBinder from './inline-dialog/binder';
 import loadingIndicator from './components/loading_indicator';
 import messages from './extensions/messages';
 import ExtensionActions from 'actions/extension_actions';
-// import resize from './resize';
-// import uiParams from '../common/ui-params';
-// import uri from '../common/uri';
 // import propagator from './propagate/rpc';
 
 /**
@@ -34,13 +25,6 @@ simpleXDM.defineModule('dialog', dialog);
 simpleXDM.defineModule('env', env);
 simpleXDM.defineModule('events', events);
 
-// rpc.extend(addons);
-// rpc.extend(dialogRpc);
-// rpc.extend(env);
-// rpc.extend(inlineDialog);
-// rpc.extend(loadingIndicator);
-// rpc.extend(messages);
-// rpc.extend(resize);
 // rpc.extend(propagator);
 
 EventDispatcher.register("extension-define-custom", function(data){
@@ -55,13 +39,6 @@ export default {
   },
   defineExtension: (name, methods) => {
     ExtensionActions.defineCustomExtension(name, methods);
-  },
-  // extend: rpc.extend,
-  // init: rpc.init,
-  // uiParams,
+  },  
   create
-  // _uriHelper: uri,
-  // _statusHelper: statusHelper,
-  // webItemHelper: content,
-  // dialog: dialog
 };
