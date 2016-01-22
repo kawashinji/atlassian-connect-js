e9183fe0e94c482ade7601b06745c9a3b1b25fa9
David Taylor <dtaylor@atlassian.com>
2016-01-22 14:31:57 +1100

    CE-283 More fullscreen dialog backflipping
    - chrome header shown iff size === 'fullscreen'
    - fix incorrect option passing in factory tests

--- tests/unit/js/dialog/dialog-factory-test.js
+++ tests/unit/js/dialog/dialog-factory-test.js
@@ -145,20 +145,20 @@ require(['ac/dialog/dialog-factory'], function(dialogFactory) {
         deepEqual(window._AP.contentResolver.resolveByParameters.args[0][0].uiParams.customData, customData)
     });
 
-    test("chrome: false option is honoured for fullscreen dialogs", function(){
+    test("chrome: false option is ignored for fullscreen dialogs", function(){
         this.server.respondWith("GET", /.*somekey\/somemodulekey/,
             [200, { "Content-Type": "text/html" }, 'This is the <span id="my-span">content</span>']);
 
         dialog = dialogFactory({
                 key: 'somekey',
                 moduleKey: 'somemodulekey',
-                id: 'dialogid',
+                id: 'dialogid'
+            }, {
                 size: "fullscreen",
                 chrome: false
-            },
-            {}, "");
+            }, "");
 
-        ok(dialogSpy.$el.hasClass('ap-aui-dialog2-chromeless'));
+        ok(!dialogSpy.$el.hasClass('ap-aui-dialog2-chromeless'));
     });
 
     test("chrome: true option is honoured for fullscreen dialogs", function(){
@@ -168,26 +168,27 @@ require(['ac/dialog/dialog-factory'], function(dialogFactory) {
         dialog = dialogFactory({
                 key: 'somekey',
                 moduleKey: 'somemodulekey',
-                id: 'dialogid',
+                id: 'dialogid'
+            }, {
                 size: "fullscreen",
                 chrome: true
-            },
-            {}, "");
+            }, "");
 
         ok(!dialogSpy.$el.hasClass('ap-aui-dialog2-chromeless'));
     });
 
-    test("chrome: undefined option defaults to false for fullscreen dialogs", function(){
+    test("chrome: undefined option defaults to false for maximised dialogs", function(){
         this.server.respondWith("GET", /.*somekey\/somemodulekey/,
             [200, { "Content-Type": "text/html" }, 'This is the <span id="my-span">content</span>']);
 
         dialog = dialogFactory({
                 key: 'somekey',
                 moduleKey: 'somemodulekey',
-                id: 'dialogid',
-                size: "fullscreen"
-            },
-            {}, "");
+                id: 'dialogid'
+
+            }, {
+                size: "maximum"
+            }, "");
 
         ok(dialogSpy.$el.hasClass('ap-aui-dialog2-chromeless'));
     });
