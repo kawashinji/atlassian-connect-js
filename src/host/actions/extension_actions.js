4c9a0d508dd17f7025f0abf486cfbec0d3a37e44
Chris Whittington <cwhittington@atlassian.com>
2016-02-01 17:26:54 +0000

    ACJS-120 keycodes plus renamed dir

--- src/host/actions/extension_actions.js
+++
@@ -1,7 +0,0 @@
-import EventDispatcher from 'dispatchers/event_dispatcher';
-
-module.exports = {
-  defineCustomExtension: function(name, methods){
-    EventDispatcher.dispatch('extension-define-custom', {name, methods});
-  }
-}
--- src/host/index.js
+++ src/host/index.js
@@ -2,13 +2,13 @@ import AnalyticsDispatcher from 'dispatchers/analytics_dispatcher';
 import EventDispatcher from 'dispatchers/event_dispatcher';
 import simpleXDM from 'simple-xdm/dist/host';
 import jwtActions from 'actions/jwt_actions';
-import events from './extensions/events';
+import events from './modules/events';
 import create from './create';
-import dialog from './extensions/dialog';
-import env from './extensions/env';
+import dialog from './modules/dialog';
+import env from './modules/env';
 import loadingIndicator from './components/loading_indicator';
-import messages from './extensions/messages';
-import ExtensionActions from 'actions/extension_actions';
+import messages from './modules/messages';
+import ModuleActions from 'actions/module_actions';
 import DomEventActions from 'actions/dom_event_actions';
 import _ from 'underscore';
 
@@ -31,7 +31,7 @@ simpleXDM.defineModule('events', events);
 
 // rpc.extend(propagator);
 
-EventDispatcher.register("extension-define-custom", function(data){
+EventDispatcher.register("module-define-custom", function(data){
   simpleXDM.defineModule(data.name, data.methods);
 });
 
@@ -64,8 +64,8 @@ export default {
       jwtActions.registerContentResolver({callback: callback});
     }
   },
-  defineExtension: (name, methods) => {
-    ExtensionActions.defineCustomExtension(name, methods);
+  defineModule: (name, methods) => {
+    ModuleActions.defineCustomModule(name, methods);
   },  
   create
 };
