c3d1f9e28369d6a920cda859435b27cdb9788f82
Chris Whittington <cwhittington@atlassian.com>
2016-02-09 13:12:15 +0000

    ACJS-152 user module compat

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,8 +1,11 @@
 //INSERT AMD STUBBER HERE!
-import amd from './amd';
+// import amd from './amd';
 import util from './util';
 import $ from './dollar';
 import consumerOptions from './consumer-options';
+
+//map AP.env.getUser to AP.user.getUser for compatibility.
+AP._hostModules.env.getUser = AP._hostModules.user.getUser;
 
 $(function(){
   console.log('sizetoparent?', consumerOptions.get('sizeToParent'));
