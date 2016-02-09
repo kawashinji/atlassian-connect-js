08e031052d34fa4dc820340d76417fb0a5459798
Chris Whittington <cwhittington@atlassian.com>
2016-02-09 13:24:46 +0000

    build

--- dist/plugin-compat.js
+++ dist/plugin-compat.js
@@ -1,126 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.APCompat = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 "use strict";
 
-// import util from './util';
-// import $ from './dollar';
-// import _events from '../common/events';
-// import base64 from '../common/base64';
-// import uri from '../common/uri';
-// import uiParams from '../common/ui-params';
-// import xdm from '../common/xdm-rpc';
-// import rpc from './rpc';
-// import events from './events';
-// import env from './env';
-
-// import messages from './messages';
-// import dialog from './dialog';
-// import inlineDialog from './inline-dialog';
-// import resize_listener from './resize_listener';
-// // pre-populate all the old core modules for the old AP.require syntax.
-
-// var modules = {
-//   '_util': {exports: util},
-//   '_dollar': {exports: $},
-//   '_events': {exports: _events},
-//   '_base64': {exports: base64},
-//   '_uri': {exports: uri},
-//   '_ui-params': {exports: uiParams},
-//   '_xdm': {exports: xdm},
-//   '_rpc': {exports: rpc},
-//   'events': {exports: events},
-//   'env': {exports: env},
-//   'messages': {exports: messages},
-//   'dialog': {exports: dialog},
-//   'inline-dialog': {exports: inlineDialog},
-//   '_resize_listener': {exports: resize_listener}
-// };
-
-// function reqAll(deps, callback) {
-//   var mods = [];
-//   var i = 0;
-//   var len = deps.length;
-//   function addOne(mod) {
-//     mods.push(mod);
-//     if (mods.length === len) {
-//       var exports = [];
-//       var i = 0;
-//       for (; i < len; i += 1) {
-//         exports[i] = mods[i].exports;
-//       }
-//       if (callback) {
-//         callback.apply(window, exports);
-//       }
-//     }
-//   }
-//   if (deps && deps.length > 0) {
-//     for (; i < len; i += 1) {
-//       reqOne(deps[i], addOne);
-//     }
-//   } else {
-//     if (callback) {
-//       callback();
-//     }
-//   }
-// }
-
-// function reqOne(name, callback) {
-//   // naive impl that assumes all modules are already loaded
-//   callback(getOrCreate(name));
-// }
-
-// function getOrCreate(name) {
-//   return modules[name] = modules[name] || {
-//     name: name,
-//     exports: function () {
-//       function exports() {
-//         var target = exports.__target__;
-//         if (target) {
-//           return target.apply(window, arguments);
-//         }
-//       }
-//       return exports;
-//     }()
-//   };
-// }
-
-// // define(name, objOrFn)
-// // define(name, deps, fn(dep1, dep2, ...))
-// export default {
-//   define: function (name, deps, exports) {
-//     var mod = getOrCreate(name);
-//     var factory;
-//     if (!exports) {
-//       exports = deps;
-//       deps = [];
-//     }
-//     if (exports) {
-//       factory = typeof exports !== 'function' ? function () {
-//         return exports;
-//       } : exports;
-//       reqAll(deps, function () {
-//         var exports = factory.apply(window, arguments);
-//         if (exports) {
-//           if (typeof exports === 'function') {
-//             mod.exports.__target__ = exports;
-//           }
-//           for (var k in exports) {
-//             if (exports.hasOwnProperty(k)) {
-//               mod.exports[k] = exports[k];
-//             }
-//           }
-//         }
-//       });
-//     }
-//   },
-//   require: function (deps, callback) {
-
-//     reqAll(typeof deps === 'string' ? [deps] : deps, callback);
-//   }
-// };
-
-},{}],2:[function(_dereq_,module,exports){
-"use strict";
-
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
@@ -190,7 +70,7 @@ var ConsumerOptions = (function () {
 
 module.exports = new ConsumerOptions();
 
-},{"./dollar":3}],3:[function(_dereq_,module,exports){
+},{"./dollar":2}],2:[function(_dereq_,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -309,15 +189,12 @@ function onDomLoad(func) {
 exports['default'] = $;
 module.exports = exports['default'];
 
-},{"./util":5}],4:[function(_dereq_,module,exports){
+},{"./util":4}],3:[function(_dereq_,module,exports){
 //INSERT AMD STUBBER HERE!
+// import amd from './amd';
 'use strict';
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-var _amd = _dereq_('./amd');
-
-var _amd2 = _interopRequireDefault(_amd);
 
 var _util = _dereq_('./util');
 
@@ -331,6 +208,9 @@ var _consumerOptions = _dereq_('./consumer-options');
 
 var _consumerOptions2 = _interopRequireDefault(_consumerOptions);
 
+//map AP.env.getUser to AP.user.getUser for compatibility.
+AP._hostModules.env.getUser = AP._hostModules.user.getUser;
+
 (0, _dollar2['default'])(function () {
   console.log('sizetoparent?', _consumerOptions2['default'].get('sizeToParent'));
   if (_consumerOptions2['default'].get('sizeToParent') === true) {
@@ -338,7 +218,7 @@ var _consumerOptions2 = _interopRequireDefault(_consumerOptions);
   }
 });
 
-},{"./amd":1,"./consumer-options":2,"./dollar":3,"./util":5}],5:[function(_dereq_,module,exports){
+},{"./consumer-options":1,"./dollar":2,"./util":4}],4:[function(_dereq_,module,exports){
 // universal iterator utility
 'use strict';
 
@@ -468,7 +348,7 @@ exports['default'] = {
 };
 module.exports = exports['default'];
 
-},{}]},{},[4])(4)
+},{}]},{},[3])(3)
 });
 
 
