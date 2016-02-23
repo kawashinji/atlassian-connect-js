f80e34f64d5be2e3946329455657e3c3e1a68955
Khanh Nguyen <khanguyen@atlassian.com>
2016-02-23 14:54:03 +1100

    ACJS-49 add AJS flags to AP

---
+++ src/host/modules/flag.js
@@ -0,0 +1,52 @@
+import $ from '../dollar';
+import _ from '../underscore';
+import EventDispatcher from 'dispatchers/event_dispatcher';
+
+const FLAGID_PREFIX = 'ap-flag-';
+const _flags = {};
+
+class Flag {
+  constructor(options, callback) {
+    const _id = FLAGID_PREFIX + callback._id;
+
+    this.flag = AJS.flag({
+      type: options.type,
+      title: options.title,
+      body: AJS.escapeHtml(options.body),
+      close: options.close
+    });
+
+    $(this.flag).attr('id', _id);
+
+    _flags[_id] = this;
+
+    $(document).on('aui-flag-close', function (e) {
+      if (e.target.id === _id) {
+        EventDispatcher.dispatch('flag-close', {id: _id});
+        _flags[_id]._destroy();
+        delete _flags[_id];
+      }
+    })
+  }
+
+  on(event, callback) {
+    const id = this.flag.id;
+    EventDispatcher.register('flag-' + event, function (data) {
+      if (data.id === id) {
+        callback();
+      }
+    });
+  }
+
+  close() {
+    this.flag.close();
+  }
+}
+
+module.exports = {
+  create: {
+    constructor: Flag,
+    on: Flag.prototype.on,
+    close: Flag.prototype.close
+  }
+}
