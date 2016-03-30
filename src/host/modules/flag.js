0785f076430cde090f8aebf2a25198b339c6c342
Khanh Nguyen <khanguyen@atlassian.com>
2016-03-30 16:08:52 +1100

    ACJS-123 add jsdoc documentation for flag and message modules

--- src/host/modules/flag.js
+++ src/host/modules/flag.js
@@ -1,3 +1,8 @@
+/**
+* Flags are the primary method for providing system feedback in the product user interface. Messages include notifications of various kinds: alerts, confirmations, notices, warnings, info and errors.
+* @module Flag
+*/
+
 import $ from '../dollar';
 import EventDispatcher from 'dispatchers/event_dispatcher';
 import FlagActions from 'actions/flag_actions';
@@ -5,6 +10,10 @@ import FlagComponent from 'components/flag';
 
 const _flags = {};
 
+/**
+* @class Flag~Flag
+* @description A flag object created by the [AP.flag]{@link module:Flag} module.
+*/
 class Flag {
   constructor(options, callback) {
 
@@ -23,6 +32,27 @@ class Flag {
     _flags[this.flag.attr('id')] = this;
   }
 
+  /**
+  * @name on
+  * @memberof Flag~Flag
+  * @method
+  * @description Binds a callback function to an event that is triggered by the Flag.
+  * @param {Event} event A flag event; currently, the only valid option is 'close'.
+  * @param {Function} callback The function that runs when the event occurs.
+  * @example
+  * // Display a nice green flag using the Flags JavaScript API.
+  * var flag = AP.flag.create({
+  *   title: 'Succesfully created a flag.',
+  *   body: 'This is a flag.',
+  *   type: 'info'
+  * });
+  *
+  * // Log stuff to the console when the flag has closed.
+  * flag.on('close', function (data) {
+  *   console.log('Flag has been closed!');
+  * })
+  *
+  */
   on(event, callback) {
     const id = this.flag.id;
     if ($.isFunction(callback)) {
@@ -30,6 +60,23 @@ class Flag {
     }
   }
 
+  /**
+  * @name close
+  * @memberof Flag~Flag
+  * @method
+  * @description Closes the Flag.
+  * @example
+  * // Display a nice green flag using the Flags JavaScript API.
+  * var flag = AP.flag.create({
+  *   title: 'Succesfully created a flag.',
+  *   body: 'This is a flag.',
+  *   type: 'info'
+  * });
+  *
+  * // Close the flag.
+  * flag.close()
+  *
+  */
   close() {
     this.flag.close();
   }
@@ -45,6 +92,24 @@ EventDispatcher.register('flag-closed', (data) => {
 });
 
 export default {
+  /**
+  * @name create
+  * @method
+  * @description Creates a new flag.
+  * @param {Object} options           Options of the flag.
+  * @param {String} options.title     The title text of the flag.
+  * @param {String} options.body      The body text of the flag.
+  * @param {String} options.type=info Sets the type of the message. Valid options are "info", "success", "warning" and "error".
+  * @param {String} options.close     The closing behaviour that this flag has. Valid options are "manual", "auto" and "never".
+  * @returns {Flag~Flag}
+  * @example
+  * // Display a nice green flag using the Flags JavaScript API.
+  * var flag = AP.flag.create({
+  *   title: 'Succesfully created a flag.',
+  *   body: 'This is a flag.',
+  *   type: 'success'
+  * });
+  */
   create: {
     constructor: Flag,
     on: Flag.prototype.on,
