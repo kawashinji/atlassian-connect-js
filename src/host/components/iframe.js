98fdf4b59e033c2f0f331961bca43726c32ded70
Chris Whittington <cwhittington@atlassian.com>
2016-04-11 13:01:06 +0100

    ACJS-211 refactor for undefined extension id

--- src/host/components/iframe.js
+++ src/host/components/iframe.js
@@ -4,10 +4,20 @@ import $ from '../dollar';
 import util from '../util';
 import simpleXDM from 'simple-xdm/dist/host';
 import urlUtil from 'utils/url';
+import JwtActions from 'actions/jwt_actions';
+import iframeUtils from 'utils/iframe';
 
 const CONTAINER_CLASSES = ['ap-container'];
 
 class Iframe {
+
+  constructor () {
+    this._contentResolver = false;
+  }
+
+  setContentResolver(callback) {
+    this._contentResolver = callback;
+  }
 
   resize(width, height, $el){
     width = util.stringToDimension(width);
@@ -20,13 +30,34 @@ class Iframe {
   }
 
   simpleXdmExtension(extension) {
-    var $iframe;
+    var iframeAttributes = iframeUtils.optionsToAttributes(extension.options);
+    extension.$el = this._renderIframe(iframeAttributes);
+
+    if(!extension.url || (urlUtil.hasJwt(extension.url) && urlUtil.isJwtExpired(extension.url))){
+      if(this._contentResolver){
+        JwtActions.requestRefreshUrl({extension: extension, resolver: this._contentResolver});
+      } else {
+        console.error('JWT is expired and no content resolver was specified');
+      }
+    } else {
+      this._simpleXdmCreate(extension);
+    }
+    IframeActions.notifyIframeCreated(extension.$el, extension);
+    return extension;
+  }
+
+  _simpleXdmCreate(extension){
     var iframeAttributes = simpleXDM.create(extension, () => {
-      IframeActions.notifyBridgeEstablished($iframe, extension);
+      IframeActions.notifyBridgeEstablished(extension.$el, extension);
     });
     extension.id = iframeAttributes.id;
-    $iframe = this._renderIframe(iframeAttributes);
-    return {$el: $iframe, extension};
+    extension.$el.attr(iframeAttributes);
+    return extension;
+  }
+
+  resolverResponse(data) {
+    data.extension.url = data.url;
+    return this._simpleXdmCreate(data.extension);
   }
 
   _renderIframe(attributes){
@@ -38,6 +69,14 @@ var IframeComponent = new Iframe();
 
 EventDispatcher.register('iframe-resize', function(data){
   IframeComponent.resize(data.width, data.height, data.$el);
+});
+
+EventDispatcher.register('content-resolver-register-by-extension', function(data){
+  IframeComponent.setContentResolver(data.callback);
+});
+
+EventDispatcher.register('jwt-url-refreshed', function(data) {
+  IframeComponent.resolverResponse(data);
 });
 
 export default IframeComponent;
--- src/host/components/iframe_container.js
+++ src/host/components/iframe_container.js
@@ -1,7 +1,4 @@
-import EventDispatcher from 'dispatchers/event_dispatcher';
 import $ from '../dollar';
-import urlUtil from 'utils/url';
-import JwtActions from 'actions/jwt_actions';
 import IframeActions from 'actions/iframe_actions';
 import IframeComponent from 'components/iframe';
 import LoadingIndicatorComponent from 'components/loading_indicator';
@@ -9,57 +6,18 @@ import LoadingIndicatorComponent from 'components/loading_indicator';
 const CONTAINER_CLASSES = ['ap-container'];
 
 class IframeContainer {
-  constructor () {
-    this._urlContainerRegistry = {};
-    this._contentResolver = false;
-  }
 
-  setContentResolver(callback) {
-    this._contentResolver = callback;
-  }
-
-  _insertIframe($container, extension) {
-    var simpleExtension = IframeComponent.simpleXdmExtension(extension);
-    $container.prepend(simpleExtension.$el);
-    if(extension.options){
-      if(extension.options.width){
-        simpleExtension.$el.css('width', extension.options.width);
-      }
-      if(extension.options.height){
-       simpleExtension.$el.css('height', extension.options.height);
-      }
-    }
-    IframeActions.notifyIframeCreated(simpleExtension.$el, simpleExtension.extension);
-    return simpleExtension;
+  _createIframe(extension) {
+    return IframeComponent.simpleXdmExtension(extension);
   }
 
   createExtension(extension, options) {
     var $container = this._renderContainer();
+    $container.append(this._createIframe(extension).$el);
     if(!options || options.loadingIndicator !== false){
       $container.append(this._renderLoadingIndicator());
     }
-    if(!extension.url || (urlUtil.hasJwt(extension.url) && urlUtil.isJwtExpired(extension.url))){
-      this._urlContainerRegistry[extension.id] = $container;
-      if(this._contentResolver){
-        JwtActions.requestRefreshUrl({extension, resolver: this._contentResolver});
-      } else {
-        console.error('JWT is expired and no content resolver was specified');
-      }
-    } else {
-      this._insertIframe($container, extension);
-    }
-
     return $container;
-  }
-
-  resolverResponse(data) {
-    var extension = data.extension;
-    var $container = this._urlContainerRegistry[extension.id];
-    if($container){
-      extension.url = data.url;
-      this._insertIframe($container, extension);
-    }
-    delete this._urlContainerRegistry[extension.id];
   }
 
   _renderContainer(attributes){
@@ -75,12 +33,5 @@ class IframeContainer {
 }
 
 var IframeContainerComponent = new IframeContainer();
-EventDispatcher.register('content-resolver-register-by-extension', function(data){
-  IframeContainerComponent.setContentResolver(data.callback);
-});
-
-EventDispatcher.register('jwt-url-refreshed', function(data) {
-  IframeContainerComponent.resolverResponse(data);
-});
 
 export default IframeContainerComponent;
