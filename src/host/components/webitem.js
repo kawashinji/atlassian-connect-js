82f7128880bcd3170c65674a8d0072a5e1bb86c0
Chris Whittington <cwhittington@atlassian.com>
2016-06-01 11:29:58 +0100

    v5 fix webitems

--- src/host/components/webitem.js
+++ src/host/components/webitem.js
@@ -51,7 +51,8 @@ class WebItem {
     var onTriggers = WebItemUtils.sanitizeTriggers(webitem.triggers);
     webitem._on = (event) => {
       event.preventDefault();
-      var $target = $(event.target);
+      // var $target = $(event.target);
+      var $target = $(event.target).closest(webitem.selector);
       var extension = {
           addon_key: WebItemUtils.getExtensionKey($target),
           key: WebItemUtils.getKey($target),
