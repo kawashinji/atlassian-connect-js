9df78c92a9608d2114aa1f8d4e05aee71b47f8ca
Khanh Nguyen <khanguyen@atlassian.com>
2016-06-06 11:34:45 +1000

    ACJS-238 refactor dialog button visibility toggle

--- src/host/components/dialog.js
+++ src/host/components/dialog.js
@@ -196,11 +196,7 @@ EventDispatcher.register('dialog-button-toggle-visibility', (data) => {
     const $button = dialog.$el.find(".aui-dialog2-footer-actions .aui-button").filter(function () {
       return $(this).data('name') === data.name;
     });
-    if (data.isHidden) {
-      $button.hide();
-    } else {
-      $button.show();
-    }
+    $button.toggle(!data.isHidden);
   }
 });
 
