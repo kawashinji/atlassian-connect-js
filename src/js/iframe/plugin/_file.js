28829c2db9ff271e1529f32e7c313c3e1251e412
Khanh Nguyen <khanguyen@atlassian.com>
2016-06-08 08:38:56 +1000

    ACJS-239 remove the console warning when enabling the file polyfill

--- src/js/iframe/plugin/_file.js
+++ src/js/iframe/plugin/_file.js
@@ -2,7 +2,6 @@ AP.define("_file", function () {
   try {
     new File([], '');
   } catch (e) {
-    console.warn('File constructor is not available. Enabled polyfill.');
     File = function (data, name, options) {
       var newFile = new Blob(data, options);
       newFile.name = name;
