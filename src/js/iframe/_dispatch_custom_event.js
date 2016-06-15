7ef5487614a523e8ebdea18de44a3222ea68b018
Marco De Jongh <mdejongh@atlassian.com>
2016-06-15 15:37:58 +1000

    ACDEV-2584 Added jsdoc for dispatchCustomEvent

--- src/js/iframe/_dispatch_custom_event.js
+++ src/js/iframe/_dispatch_custom_event.js
@@ -1,9 +1,9 @@
 ( (typeof _AP !== "undefined") ? define : AP.define)("_dispatch_custom_event", [], function () {
     /**
-     *
-     * @param target
-     * @param eventName
-     * @returns {*|boolean}
+     * Dispatches a custom event to the given target element
+     * @param target {Object} The DOM node to which to dispatch the event to
+     * @param eventName {String} The name of the event
+     * @returns {Boolean} false if at least one of the event handlers which handled this event called Event.preventDefault(). Otherwise it returns true.
      */
      return function dispatchCustomEvent(target, eventName) {
         var event;
