b9e3e38b01ad982da00682814573c2212bb56610
Marco De Jongh <mdejongh@atlassian.com>
2016-06-15 15:52:05 +1000

    ACDEV-2584 Removed duplicate debounce method

--- src/js/iframe/plugin/resize.js
+++ src/js/iframe/plugin/resize.js
@@ -1,27 +1,11 @@
-AP.require(["_dollar", "_rpc"], function ($, rpc) {
+AP.require(["_dollar", "_rpc", "_util"], function ($, rpc, util) {
     "use strict";
-
-    function debounce(func, wait) {
-        var timeout;
-        var result;
-        return function () {
-            var args = arguments;
-            var context = this;
-            var later = function () {
-                result = func.apply(context, args);
-                context = args = null;
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            return result;
-        };
-    }
-
+    
     rpc.extend(function () {
         return {
             initForFrame: function (config, xdm) {
                 if (xdm) {
-                    xdm.resize = debounce(function resize ($, width, height) {
+                    xdm.resize = util.debounce(function resize ($, width, height) {
                         $(this.iframe).each(function(i, el) {
                             el.style.width = width;
                             el.style.height = height + 'px';
