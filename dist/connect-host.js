48a99cd9011cd78aadc4043c1cf01224cb9a638d
Khanh Nguyen <khanguyen@atlassian.com>
2016-08-18 10:07:08 -0700

    ACJS-331 redist

--- dist/connect-host.js
+++ dist/connect-host.js
@@ -5404,7 +5404,7 @@
 	 * Add version
 	 */
 	if (!window._AP.version) {
-	  window._AP.version = '5.0.0-beta.4';
+	  window._AP.version = '5.0.0-beta.5';
 	}
 
 	host.defineModule('messages', messages);
