37ff019540e15d5dde92d63cbd2008622bc145b4
Chris Whittington <cwhittington@atlassian.com>
2016-11-21 11:56:07 +1100

    ACJS-459 bump simple-xdm version

--- dist/iframe.js
+++ dist/iframe.js
@@ -1373,6 +1373,14 @@ var AP = (function () {
 
     expand.addEventListener('scroll', onScroll);
     shrink.addEventListener('scroll', onScroll);
+
+    var observerConfig = {
+      attributes: true,
+      attributeFilter: ['style']
+    };
+
+    var observer = new MutationObserver(onScroll);
+    observer.observe(element, observerConfig);
   }
 
   var resizeListener = {
@@ -1394,26 +1402,48 @@ var AP = (function () {
     function AutoResizeAction(callback) {
       classCallCheck(this, AutoResizeAction);
 
-      this.resizeStore = [];
+      this.dimensionStores = {
+        width: [],
+        height: []
+      };
       this.callback = callback;
     }
 
     createClass(AutoResizeAction, [{
+      key: '_setVal',
+      value: function _setVal(val, type, time) {
+        this.dimensionStores[type] = this.dimensionStores[type].filter(function (entry) {
+          return time - entry.setAt < 400;
+        });
+        this.dimensionStores[type].push({
+          val: parseInt(val, 10),
+          setAt: time
+        });
+      }
+    }, {
+      key: '_isFlicker',
+      value: function _isFlicker(val, type) {
+        return this.dimensionStores[type].length >= 5;
+      }
+    }, {
       key: 'triggered',
       value: function triggered(dimensions) {
         dimensions = dimensions || size();
         var now = Date.now();
-        dimensions.setAt = now;
-        this.resizeStore = this.resizeStore.filter(function (entry) {
-          return now - entry.setAt < 1000;
-        });
-        this.resizeStore.push(dimensions);
-        if (this.resizeStore.length === 3) {
-          var oldDimensions = this.resizeStore[0];
-          this.resizeStore = this.resizeStore.slice(1);
-          if (dimensions.w <= oldDimensions.w && dimensions.h <= oldDimensions.h) {
-            return;
-          }
+        this._setVal(dimensions.w, 'width', now);
+        this._setVal(dimensions.h, 'height', now);
+        var isFlickerWidth = this._isFlicker(dimensions.w, 'width', now);
+        var isFlickerHeight = this._isFlicker(dimensions.h, 'height', now);
+        if (isFlickerWidth) {
+          dimensions.w = "100%";
+          console.error("SIMPLE XDM: auto resize flickering width detected, setting to 100%");
+        }
+        if (isFlickerHeight) {
+          var vals = this.dimensionStores['height'].map(function (x) {
+            return x.val;
+          });
+          dimensions.h = Math.max.apply(null, vals) + 'px';
+          console.error("SIMPLE XDM: auto resize flickering height detected, setting to: " + dimensions.h);
         }
         this.callback(dimensions.w, dimensions.h);
       }
