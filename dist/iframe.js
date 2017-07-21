9b173846d7900c89d177a8234e6e0578af67e91f
David Boyd <dboyd@atlassian.com>
2017-07-21 12:01:46 +1000

    prepare release 5.1.7

--- dist/iframe.js
+++ dist/iframe.js
@@ -1399,7 +1399,7 @@ var AP = (function () {
       _this._eventHandlers = {};
       _this._pendingCallbacks = {};
       _this._keyListeners = [];
-      _this._version = "5.1.6";
+      _this._version = "5.1.7";
       _this._apiTampered = undefined;
       _this._isSubIframe = _this._topHost !== window.parent;
       _this._onConfirmedFns = [];
