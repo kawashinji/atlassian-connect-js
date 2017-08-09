6212ca07595576e67e5471a59b489646acfdb407
Chris Whittington <cwhittington@atlassian.com>
2017-08-09 06:33:07 +0100

    ACJS-707 bump dist

--- dist/connect-host.js
+++ dist/connect-host.js
@@ -5513,7 +5513,7 @@
 	 * Add version
 	 */
 	if (!window._AP.version) {
-	  window._AP.version = '5.1.11';
+	  window._AP.version = '5.1.12';
 	}
 
 	simpleXDM$1.defineModule('messages', messages);
