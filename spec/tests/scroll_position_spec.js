1246b7eb2393026a9434067c4e8bcab0fb054887
Chris Whittington <cwhittington@atlassian.com>
2018-04-05 16:27:19 +0100

    ACJS-895 tests

--- spec/tests/scroll_position_spec.js
+++ spec/tests/scroll_position_spec.js
@@ -4,39 +4,15 @@ beforeEach(() => {
   $('iframe').remove();
 });
 describe('scroll position', () => {
-  // it('gets the scroll position of parent page', (done) => {
-  //   var elementId = Math.random().toString(36).substring(2, 8);
-  //   var callback = function (position) {
-  //     expect(position).toEqual({
-  //       scrollY: 900,
-  //       scrollX: -100,
-  //       width: window.innerWidth,
-  //       height: window.innerHeight
-  //     });
-  //     done();
-  //   };
-  //   callback._context = {
-  //     extension: {
-  //       options: {
-  //         isFullPage: true
-  //       }
-  //     },
-  //     extension_id: elementId
-  //   };
-  //   $('<iframe>').attr({id: elementId}).appendTo('body');
-  //   $('body').css({
-  //     height: 2000,
-  //     margin: 100
-  //   });
-  //   window.scrollTo(0, 1000);
-  //   ScrollPosition.getPosition(callback);
-  // });
-
-  it('sets the scroll position of the page', (done) => {
+  it('gets the scroll position of parent page', (done) => {
     var elementId = Math.random().toString(36).substring(2, 8);
-    var scrollPosition = 10;
     var callback = function (position) {
-      expect(position.scrollY).toEqual(scrollPosition);
+      expect(position).toEqual({
+        scrollY: 900,
+        scrollX: -100,
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
       done();
     };
     callback._context = {
@@ -47,16 +23,40 @@ describe('scroll position', () => {
       },
       extension_id: elementId
     };
-    $('<iframe>').attr({id: elementId}).css({
-      width: '300px',
-      height: '2000px'
-    }).appendTo('body');
-    window.scrollTo(0, 0);
-    ScrollPosition.setVerticalPosition(scrollPosition, callback);
-    // expect(document.documentElement.scrollTop).toEqual(scrollPosition);
-    setTimeout(function(){
-      ScrollPosition.getPosition(callback);
-    }, 1000);
-
+    $('<iframe>').attr({id: elementId}).appendTo('body');
+    $('body').css({
+      height: 2000,
+      margin: 100
+    });
+    window.scrollTo(0, 1000);
+    ScrollPosition.getPosition(callback);
   });
+
+  // it('sets the scroll position of the page', (done) => {
+  //   var elementId = Math.random().toString(36).substring(2, 8);
+  //   var scrollPosition = 10;
+  //   var callback = function (position) {
+  //     expect(position.scrollY).toEqual(scrollPosition);
+  //     done();
+  //   };
+  //   callback._context = {
+  //     extension: {
+  //       options: {
+  //         isFullPage: true
+  //       }
+  //     },
+  //     extension_id: elementId
+  //   };
+  //   $('<iframe>').attr({id: elementId}).css({
+  //     width: '300px',
+  //     height: '2000px'
+  //   }).appendTo('body');
+  //   window.scrollTo(0, 0);
+  //   ScrollPosition.setVerticalPosition(scrollPosition, callback);
+  //   // expect(document.documentElement.scrollTop).toEqual(scrollPosition);
+  //   setTimeout(function(){
+  //     ScrollPosition.getPosition(callback);
+  //   }, 1000);
+
+  // });
 });
