1472cd5fd08a57ece51375d2b546148b3cabf04f
Chris Whittington <cwhittington@atlassian.com>
2018-04-10 14:49:23 +0100

    ACJS-896 scroll events

--- dist/connect-host.js
+++ dist/connect-host.js
@@ -5176,6 +5176,46 @@
 	  }
 	};
 
+	var TRIGGER_PERCENTAGE = 10; //% before scroll events are fired
+	var activeGeneralPageAddon = void 0;
+
+	EventDispatcher$1.register('iframe-bridge-established', function (extension) {
+	  if (extension.options.isFullPage) {
+	    window.addEventListener('scroll', scrollEventHandler);
+	    activeGeneralPageAddon = extension.extension_id;
+	  }
+	});
+
+	EventDispatcher$1.register('iframe-destroyed', function (extension) {
+	  removeScrollEvent();
+	});
+
+	EventDispatcher$1.register('iframe-unload', function (extension) {
+	  removeScrollEvent();
+	});
+
+	function removeScrollEvent() {
+	  window.removeEventListener('scroll', scrollEventHandler);
+	  activeGeneralPageAddon = undefined;
+	}
+
+	function scrollEventHandler() {
+	  var documentHeight = document.documentElement.scrollHeight;
+	  var windowHeight = window.innerHeight;
+
+	  var boundary = documentHeight * (TRIGGER_PERCENTAGE / 100);
+	  if (window.scrollY <= boundary) {
+	    triggerEvent('nearTop');
+	  }
+	  if (windowHeight + window.scrollY + boundary >= documentHeight) {
+	    triggerEvent('nearBottom');
+	  }
+	}
+
+	function triggerEvent(type) {
+	  EventActions.broadcast('scroll.' + type, { extension_id: activeGeneralPageAddon }, {});
+	}
+
 	var scrollPosition = {
 	  /**
 	   * Get's the scroll position relative to the browser viewport
--- src/host/modules/scroll-position.js
+++ src/host/modules/scroll-position.js
@@ -1,5 +1,47 @@
 import util from '../util';
 import $ from '../dollar';
+import EventDispatcher from '../dispatchers/event_dispatcher';
+import EventActions from '../actions/event_actions';
+
+const TRIGGER_PERCENTAGE = 10; //% before scroll events are fired
+let activeGeneralPageAddon;
+
+EventDispatcher.register('iframe-bridge-established', function(extension) {
+  if(extension.options.isFullPage) {
+    window.addEventListener('scroll', scrollEventHandler);
+    activeGeneralPageAddon = extension.extension_id;
+  }
+});
+
+EventDispatcher.register('iframe-destroyed', function(extension) {
+  removeScrollEvent();
+});
+
+EventDispatcher.register('iframe-unload', function(extension) {
+  removeScrollEvent();
+});
+
+function removeScrollEvent(){
+  window.removeEventListener('scroll', scrollEventHandler);
+  activeGeneralPageAddon = undefined;
+}
+
+function scrollEventHandler(){
+  var documentHeight = document.documentElement.scrollHeight;
+  var windowHeight = window.innerHeight;
+
+  var boundary = documentHeight * (TRIGGER_PERCENTAGE/100);
+  if(window.scrollY <= boundary) {
+    triggerEvent('nearTop');
+  }
+  if((windowHeight + window.scrollY + boundary) >= documentHeight) {
+    triggerEvent('nearBottom');
+  }
+}
+
+function triggerEvent(type) {
+  EventActions.broadcast('scroll.' + type, {extension_id: activeGeneralPageAddon}, {});
+}
 
 export default {
   /**
