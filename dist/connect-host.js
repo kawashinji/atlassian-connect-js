4d600b187a6823c9a491412c195fb62298911af1
Chris Whittington <cwhittington@atlassian.com>
2018-05-20 20:52:30 +0100

    bumps

--- dist/connect-host.js
+++ dist/connect-host.js
@@ -2495,49 +2495,61 @@
 	  revLookup[code.charCodeAt(i)] = i;
 	}
 
+	// Support decoding URL-safe base64 strings, as Node.js does.
+	// See: https://en.wikipedia.org/wiki/Base64#URL_applications
 	revLookup['-'.charCodeAt(0)] = 62;
 	revLookup['_'.charCodeAt(0)] = 63;
 
-	function placeHoldersCount(b64) {
+	function getLens(b64) {
 	  var len = b64.length;
+
 	  if (len % 4 > 0) {
 	    throw new Error('Invalid string. Length must be a multiple of 4');
 	  }
 
-	  // the number of equal signs (place holders)
-	  // if there are two placeholders, than the two characters before it
-	  // represent one byte
-	  // if there is only one, then the three characters before it represent 2 bytes
-	  // this is just a cheap hack to not do indexOf twice
-	  return b64[len - 2] === '=' ? 2 : b64[len - 1] === '=' ? 1 : 0;
+	  // Trim off extra bytes after placeholder bytes are found
+	  // See: https://github.com/beatgammit/base64-js/issues/42
+	  var validLen = b64.indexOf('=');
+	  if (validLen === -1) validLen = len;
+
+	  var placeHoldersLen = validLen === len ? 0 : 4 - validLen % 4;
+
+	  return [validLen, placeHoldersLen];
+	}
+
+	function _byteLength(b64, validLen, placeHoldersLen) {
+	  return (validLen + placeHoldersLen) * 3 / 4 - placeHoldersLen;
 	}
 
 	function toByteArray(b64) {
-	  var i, j, l, tmp, placeHolders, arr;
-	  var len = b64.length;
-	  placeHolders = placeHoldersCount(b64);
+	  var tmp;
+	  var lens = getLens(b64);
+	  var validLen = lens[0];
+	  var placeHoldersLen = lens[1];
 
-	  arr = new Arr(len * 3 / 4 - placeHolders);
+	  var arr = new Arr(_byteLength(b64, validLen, placeHoldersLen));
+
+	  var curByte = 0;
 
 	  // if there are placeholders, only get up to the last complete 4 chars
-	  l = placeHolders > 0 ? len - 4 : len;
+	  var len = placeHoldersLen > 0 ? validLen - 4 : validLen;
 
-	  var L = 0;
-
-	  for (i = 0, j = 0; i < l; i += 4, j += 3) {
+	  for (var i = 0; i < len; i += 4) {
 	    tmp = revLookup[b64.charCodeAt(i)] << 18 | revLookup[b64.charCodeAt(i + 1)] << 12 | revLookup[b64.charCodeAt(i + 2)] << 6 | revLookup[b64.charCodeAt(i + 3)];
-	    arr[L++] = tmp >> 16 & 0xFF;
-	    arr[L++] = tmp >> 8 & 0xFF;
-	    arr[L++] = tmp & 0xFF;
+	    arr[curByte++] = tmp >> 16 & 0xFF;
+	    arr[curByte++] = tmp >> 8 & 0xFF;
+	    arr[curByte++] = tmp & 0xFF;
 	  }
 
-	  if (placeHolders === 2) {
+	  if (placeHoldersLen === 2) {
 	    tmp = revLookup[b64.charCodeAt(i)] << 2 | revLookup[b64.charCodeAt(i + 1)] >> 4;
-	    arr[L++] = tmp & 0xFF;
-	  } else if (placeHolders === 1) {
+	    arr[curByte++] = tmp & 0xFF;
+	  }
+
+	  if (placeHoldersLen === 1) {
 	    tmp = revLookup[b64.charCodeAt(i)] << 10 | revLookup[b64.charCodeAt(i + 1)] << 4 | revLookup[b64.charCodeAt(i + 2)] >> 2;
-	    arr[L++] = tmp >> 8 & 0xFF;
-	    arr[L++] = tmp & 0xFF;
+	    arr[curByte++] = tmp >> 8 & 0xFF;
+	    arr[curByte++] = tmp & 0xFF;
 	  }
 
 	  return arr;
