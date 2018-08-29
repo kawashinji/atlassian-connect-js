9ccf039783370607ccf3b6638d3aeadf551b2bfe
Chris Whittington <cwhittington@atlassian.com>
2018-08-29 08:53:09 +0100

    5.1.69 release [skip ci]

--- dist/connect-host.js
+++ dist/connect-host.js
@@ -6085,7 +6085,7 @@
 	 * Add version
 	 */
 	if (!window._AP.version) {
-	  window._AP.version = '5.1.68';
+	  window._AP.version = '5.1.69';
 	}
 
 	simpleXDM$1.defineModule('messages', messages);
