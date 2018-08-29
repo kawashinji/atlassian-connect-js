e05d990087cb8d9be09de2ad91ae4288d12f2c57
Dugald Morrow <>
2018-08-29 12:22:46 +1000

    ACJS-1013: Fixed setting the iframeIsCacheable analytics attribute.

--- src/host/dispatchers/analytics_dispatcher.js
+++ src/host/dispatchers/analytics_dispatcher.js
@@ -63,7 +63,7 @@ class AnalyticsDispatcher {
   trackLoadingEnded(extension) {
     if(this._addons && extension && this._addons[extension.id]) {
       var href = extension.url;
-      var iframeIsCacheable = href && href.indexOf('xdm_e=') === -1;
+      var iframeIsCacheable = href !== undefined && href.indexOf('xdm_e=') === -1;
       var value = this._time() - this._addons[extension.id].startLoading;
       var iframeLoadApdex = this.getIframeLoadApdex(value);
       this._track('iframe.performance.load', {
