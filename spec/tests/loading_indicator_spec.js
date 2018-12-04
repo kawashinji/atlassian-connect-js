1865ff3a93ee61e7a2f4557e261dafb9bc755f05
Chris Whittington <cwhittington@atlassian.com>
2018-12-04 14:05:09 +0000

    ACJS-1050 test timeout

--- spec/tests/loading_indicator_spec.js
+++ spec/tests/loading_indicator_spec.js
@@ -1,6 +1,6 @@
 import LoadingIndicatorComponent from 'src/host/components/loading_indicator';
 import EventDispatcher from 'src/host/dispatchers/event_dispatcher';
-
+import IframeActions from 'src/host/actions/iframe_actions';
 
 describe('Loading indicator component', () => {
   var loadingIndicator;
@@ -35,6 +35,70 @@ describe('Loading indicator component', () => {
     $container.append(loadingIndicator);
     LoadingIndicatorComponent.timeout($container, extensionId);
     expect($container.find('a.ap-btn-cancel').length).toEqual(1);
+  });
+
+  describe('loading timeout test', () => {
+    var $timeoutLoadingIndicator;
+    var $timeoutIframeContainer;
+    var $timeoutLoadingIndicator;
+    var timeoutExtension = {
+      id: 'abc123-loading',
+      addon_key: 'some-addon-key-loading',
+      key: 'some-module-key-loading',
+      $el: false,
+      options: {}
+    };
+
+    beforeEach(() => {
+      $timeoutIframeContainer = $('<div />').addClass('ap-iframe-container');
+      $timeoutLoadingIndicator = LoadingIndicatorComponent.render();
+      $timeoutIframeContainer.append($timeoutLoadingIndicator);
+      timeoutExtension.$el = $('<iframe />');
+      $timeoutIframeContainer.append(timeoutExtension.$el);
+      jasmine.clock().install();
+      $timeoutIframeContainer.appendTo('body');
+    });
+
+    afterEach(() => {
+      jasmine.clock().uninstall();
+      $timeoutIframeContainer.remove();
+    });
+
+    it('will trigger timeout if loaded too late', () => {
+      var timeoutEventSpy = jasmine.createSpy('spy');
+      EventDispatcher.registerOnce('iframe-bridge-timeout', timeoutEventSpy);
+      console.log('timeout extension????', timeoutExtension, timeoutExtension.$el);
+      EventDispatcher.dispatch('iframe-create', {
+        extension: timeoutExtension,
+        $el: timeoutExtension.$el
+      });
+
+      expect(timeoutEventSpy).not.toHaveBeenCalled();
+      jasmine.clock().tick(12001);
+      IframeActions.notifyBridgeEstablished(timeoutExtension.$el, timeoutExtension);
+      // EventDispatcher.dispatch('iframe-bridge-established', {});
+      expect(timeoutEventSpy).toHaveBeenCalled();
+    });
+
+    // it('will NOT trigger timeout if loaded within load timeout', () => {
+    //   var timeoutEventSpy = jasmine.createSpy('spy');
+    //   EventDispatcher.registerOnce('iframe-bridge-timeout', timeoutEventSpy);
+
+    //   EventDispatcher.dispatch('iframe-create', {
+    //     extension: timeoutExtension,
+    //     $el: timeoutExtension.$el
+    //   });
+
+    //   expect(timeoutEventSpy).not.toHaveBeenCalled();
+    //   jasmine.clock().tick(100);
+    //   IframeActions.notifyBridgeEstablished(timeoutExtension.$el, timeoutExtension);
+    //   jasmine.clock().tick(12001);
+    //   expect(timeoutEventSpy).not.toHaveBeenCalled();
+    // });
+
+
+    // make another test using iframe-bridge-established to check for success case
+
   });
 
   it('will cancel', () => {
