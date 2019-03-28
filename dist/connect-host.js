22c3185f1af7f6643d3412fcb8a982959a65a675
David Boyd <dboyd@atlassian.com>
2019-03-28 11:10:22 +0100

    add compiled dist

--- dist/connect-host.js
+++ dist/connect-host.js
@@ -862,31 +862,6 @@
 	  }
 	});
 
-	// Temporary for ACJS-1028 1 week experiment
-	function getFeatureFlag(flagName, defaultValue) {
-	  var flagMeta = document.querySelector('meta[name="ajs-fe-feature-flags"]');
-	  if (!flagMeta) {
-	    return defaultValue;
-	  }
-
-	  var flagContent = flagMeta.getAttribute('content');
-	  if (!flagContent) {
-	    return defaultValue;
-	  }
-
-	  var flagJson = {};
-	  try {
-	    flagJson = JSON.parse(flagContent);
-	  } catch (err) {
-	    return defaultValue;
-	  }
-
-	  if (!flagJson[flagName] || typeof flagJson[flagName].value !== 'boolean') {
-	    return defaultValue;
-	  }
-	  return flagJson[flagName].value;
-	}
-
 	var EVENT_NAME_PREFIX = 'connect.addon.';
 
 	/**
@@ -960,8 +935,7 @@
 	        iframeLoadMillis: value,
 	        iframeLoadApdex: iframeLoadApdex,
 	        iframeIsCacheable: iframeIsCacheable,
-	        value: value > LOADING_TIME_THRESHOLD ? 'x' : Math.ceil(value / LOADING_TIME_TRIMP_PRECISION),
-	        dnsPrefetching: getFeatureFlag('connect-app-dns-prefetching', false)
+	        value: value > LOADING_TIME_THRESHOLD ? 'x' : Math.ceil(value / LOADING_TIME_TRIMP_PRECISION)
 	      });
 	    } else {
 	      console.error('ACJS: cannot track loading end analytics', this._addons, extension);
