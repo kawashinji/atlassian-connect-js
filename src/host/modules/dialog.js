ee0e5757eddd200fe6766ce5f2babe12e6ca1878
Denys Prokhorin <dprokhorin@atlassian.com>
2020-07-09 16:25:34 +0300

    DOCS-3475:Change link to dialog component

--- src/host/modules/dialog.js
+++ src/host/modules/dialog.js
@@ -336,7 +336,7 @@ class CreateButton {
  *
  * To do that, you'll need to add the AUI styles to your dialog.
  *
- * For more information, read about the Atlassian User Interface [dialog component](https://docs.atlassian.com/aui/latest/docs/dialog.html).
+ * For more information, read about the Atlassian User Interface [dialog component](https://docs.atlassian.com/aui/latest/docs/dialog2.html).
  * @exports Dialog
  */
 export default {
