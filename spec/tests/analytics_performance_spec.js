b84daf5b384eb549e4fe2a126f4dc405d655d39e
Chris Whittington <cwhittington@atlassian.com>
2021-07-07 11:38:15 +0200

    FORTIFIED-79 skip safari

--- spec/tests/analytics_performance_spec.js
+++ spec/tests/analytics_performance_spec.js
@@ -17,7 +17,8 @@ describe('Plugin Performance Analytics', () => {
     expect(AP._analytics.trackIframePerformanceMetrics).toHaveBeenCalled();
     const firstCallArg = AP._analytics.trackIframePerformanceMetrics.calls.first().args[0];
     // We skip these assertions for the p2 plugin integration test version of chrome used in webdriver tests because the API does not exist in that version.
-    if(navigator.appVersion.indexOf('Chrome/48.') === -1) {
+    // We also skip these in Safari because Safari does not support this API.
+    if(navigator.appVersion.indexOf('Chrome/48.') === -1 && navigator.appVersion.indexOf('Safari') === -1) {
       expect(firstCallArg.connectionTime).toEqual(jasmine.any(Number));
       expect(firstCallArg.decodedBodySize).toEqual(jasmine.any(Number));
       expect(firstCallArg.domContentLoadedTime).toEqual(jasmine.any(Number));
